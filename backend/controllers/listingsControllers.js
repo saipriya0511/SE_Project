b2ebcaf86dfc671e0a56423f17e2be57726c34ec
sailalith0007 <sailalith2000@gmail.com>
2024-10-15 11:28:04 -0500

    created add listing Api endpoint in the server

---
+++ backend/controllers/listingsControllers.js
@@ -0,0 +1,49 @@
+const Listings = require("../Models/ListingsModel");
+
+async function addListing(req, res) {
+  const {
+    community,
+    location,
+    roomsCount,
+    houseArea,
+    houseWidth,
+    BathRoomCount,
+    lookingForCount,
+    description,
+  } = req.body;
+
+  try {
+    if (
+      !community ||
+      !location ||
+      !roomsCount ||
+      !houseArea ||
+      !houseWidth ||
+      !BathRoomCount ||
+      !lookingForCount ||
+      !description
+    ) {
+      return res
+        .status(200)
+        .json({ EnterAllDetails: "Please fill all the fields" });
+    }
+
+    const data = new Listings({
+      community,
+      location,
+      roomsCount,
+      description,
+      houseArea,
+      houseWidth,
+      BathRoomCount,
+      lookingForCount,
+    });
+    const d = await data.save();
+    if(d) {
+      return res.status(200).json({ Added: "Listing added successfully" });
+    }
+    return res.json(d);
+  } catch (error) {
+    console.log(error);
+  }
+}
