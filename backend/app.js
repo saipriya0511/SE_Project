ebfe1c3054d1762bc1cc6b6c157a13884151a5ac
HarshaVardhanRepudi <harshavardhanrepudi123@gmail.com>
2024-11-05 21:09:24 +0530

    fixed server app routes

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
-const express = require("express")
-const app = express()
-
-const dotenv = require("dotenv")
-dotenv.config()
-
-const cors = require("cors")
-app.use(cors())
-
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
  
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./db");
+
+const AuthRoutes = require("./Routes/AuthRoutes");
+app.use("/api", AuthRoutes);
+
+const OtpRouter = require("./Routes/OtpRoutes");
+app.use("/api", OtpRouter);
+
+const ListingsRoutes = require("./Routes/ListingsRoutes");
+app.use("/api", ListingsRoutes);
+
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-  });
+});
--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 
-const { addListing, getAllListings } = require("../Controllers/ListingsController");
+const { addListing, getAllListings, getPlaceDetails } = require("../Controllers/ListingsController");
+const { uploadFile } = require("../s3");
+const { bookUpload } = require("../multer");
 
 
-
+ 
 router.get("/all-listings", getAllListings);
-router.post("/add-listing", addListing);
+router.post("/add-listing",bookUpload, addListing);
+
+router.post("/autocomplete", getPlaceDetails);
 
 
 
