524371c7e5a2096801584930b5bb73f9dd5af757
HimaHarshini1 <himahd2024@gmail.com>
2024-11-21 21:47:53 -0600

    created a api endpoint to get communities by search

--- backend/controllers/communities.js
+++ backend/controllers/communities.js
@@ -41,5 +41,29 @@ const addReview = async (req, res) => {
     }
 };
 
+const getCommunitiesBySearch = async (req, res) => {
+    try {
+        const { roomsCount, bathroomCount, lookingForCount, distance, price } = req.query;
+
+        // Build query object dynamically based on provided filters
+        const query = {};
+        if (roomsCount) query.roomsCount = roomsCount === "more than 5" ? { $gt: 5 } : parseInt(roomsCount);
+        if (bathroomCount) query.bathroomCount = bathroomCount === "more than 5" ? { $gt: 5 } : parseInt(bathroomCount);
+        if (lookingForCount) query.lookingForCount = lookingForCount === "more than 5" ? { $gt: 5 } : parseInt(lookingForCount);
+        if (distance) query.distance = { $lte: parseFloat(distance) };
+        if (price) query.price = { $lte: parseFloat(price) };
+
+        const searchedListings = await CommunitiyModel.find(query);
+
+        if (searchedListings.length === 0) {
+            return res.status(200).json({ noResults: "No results found for the selected search criteria." });
+        }
+
+       return res.status(200).json({searchedListings: searchedListings});
+    } catch (error) {
+        console.error("Error fetching communities:", error);
+        res.status(500).json({ message: "Error fetching communities" });
+    }
+};
 
 module.exports = {getCommunities}
