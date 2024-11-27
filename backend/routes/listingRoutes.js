04d76c1cab30a3a38fdaf16a4a2304afc58a434d
HimaHarshini1 <himahd2024@gmail.com>
2024-11-27 02:04:12 -0600

    updated listing api endpoint according to the new requirements

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -2,16 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 
-const { addListing, getAllListings, getPlaceDetails } = require("../Controllers/ListingsController");
+const { addListing, getAllListings, getPlaceDetails, getListingsBySearch, addHouseSearchHistory, sendMatchedListingsEmail, getListingsByUserId, getListingById, updateListing } = require("../Controllers/ListingsController");
 const { uploadFile } = require("../s3");
 const { bookUpload } = require("../multer");
 
-
  
 router.get("/all-listings", getAllListings);
+
+router.get("/listing/:id", getListingById);
+
 router.post("/add-listing",bookUpload, addListing);
 
 router.post("/autocomplete", getPlaceDetails);
+
+router.get("/all-listings/search", getListingsBySearch);
+
+router.post("/listings-search-history", addHouseSearchHistory);
+
+router.get("/mail", sendMatchedListingsEmail);
+
+router.get("/user-listings/:userId", getListingsByUserId);
+
+router.put("/update-listing/:id", bookUpload, updateListing);
 
 
 
