d619a1434465acbd2a00f031f4879a23686cda30
saipriya0511 <161508620+saipriya0511@users.noreply.github.com>
2024-12-01 19:40:56 +0530

    Update before_install.sh

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -3,6 +3,7 @@
 # Install LTS Node
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt install -y nodejs
+sudo apt install -y npm
 # Install pm2
 sudo npm install pm2 -g
 # make project directory
