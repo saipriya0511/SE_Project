30eda1e9a7ededfb8909f21bdfa69c4c0a7eaff1
saipriya0511 <161508620+saipriya0511@users.noreply.github.com>
2024-12-01 18:44:59 +0530

    Create before_install.sh

---
+++ codedeploy/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install LTS Node
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+# Install pm2
+sudo npm install pm2 -g
+# make project directory
+mkdir -p /home/ubuntu/se-project
